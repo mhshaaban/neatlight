f48aedd9d0b5862fa04d4766c169046d5ea229c4
David McClure <david.mcclure@virginia.edu>
2013-10-16 15:48:32 -0700

    Basic styling for GA text.

--- neatline/exhibits/themes/gettysburg-address/show.php
+++ neatline/exhibits/themes/gettysburg-address/show.php
@@ -24,13 +24,7 @@
 
 <div id="neatline-narrative" class="narrative">
 
-  <!-- Back Link -->
-  <a href="<?php echo url('neatline'); ?>" class="back">
-    <span class="arrow">&larr;</span> View all Exhibits
-  </a>
-
   <!-- Narrative -->
-  <h1 class="title"><?php echo nl_getExhibitField('title'); ?></h1>
   <?php echo nl_getExhibitField('narrative'); ?>
 
   <!-- Colophon -->
