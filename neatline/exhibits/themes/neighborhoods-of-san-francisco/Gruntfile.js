a1a9ee34ba8c5cb10db38b21fde03621f34150da
David McClure <david.mcclure@virginia.edu>
2013-11-25 20:48:49 -0800

    Adds shell for SF exhibit theme.

---
+++ neatline/exhibits/themes/neighborhoods-of-san-francisco/Gruntfile.js
@@ -0,0 +1,50 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.initConfig({
+
+    uglify: {
+      dist: {
+        files: {
+          'script.js': [
+            'assets/javascripts/*.js',
+            'bower_components/nprogress/nprogress.js'
+          ]
+        }
+      }
+    },
+
+    stylus: {
+      dist: {
+        files: {
+          'style.css': [
+            'assets/stylesheets/*.styl',
+            'bower_components/nprogress/nprogress.css'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      dist: {
+        files: ['assets/**/*'],
+        tasks: ['stylus', 'uglify']
+      }
+    }
+
+  });
+
+};
