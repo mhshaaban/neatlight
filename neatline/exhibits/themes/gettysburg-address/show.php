7b6b226e1797f61d0e6e094e1b8d92bd922ba119
David McClure <david.mcclure@virginia.edu>
2014-01-23 13:30:34 -0800

    Standardizes doc-blocks in Gettysburg sub-theme.

--- neatline/exhibits/themes/gettysburg-address/show.php
+++ neatline/exhibits/themes/gettysburg-address/show.php
@@ -1,6 +1,15 @@
+<?php
 
-<!-- vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; -->
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; */
 
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+?>
 
 <?php queue_css_file('payloads/style'); ?>
 <?php queue_js_file('show'); ?>
