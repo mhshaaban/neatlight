6149899c1af8a8c4b6014994cc1a038bdae6b37c
David McClure <david.mcclure@virginia.edu>
2014-02-24 18:10:24 -0800

    Sets up shell for gemini theme.

---
+++ neatline/exhibits/themes/gemini-over-baja-california/Gruntfile.js
@@ -0,0 +1,71 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+
+    concat: {
+      dist: {
+        src: [
+          'bower_components/nprogress/nprogress.js',
+          'assets/javascripts/*.js'
+        ],
+        dest: 'script.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        src: '<%= concat.dist.src %>',
+        dest: '<%= concat.dist.dest %>'
+      }
+    },
+
+    stylus: {
+      dist: {
+        files: {
+          'style.css': 'assets/stylesheets/*.styl'
+        }
+      },
+      options: {
+        paths: ['bower_components'],
+        'include css': true
+      }
+    },
+
+    cssmin: {
+      dist: {
+        files: {
+          'style.css': 'style.css'
+        }
+      }
+    },
+
+    watch: {
+      dist: {
+        files: 'assets/**/*',
+        tasks: 'compile'
+      }
+    }
+
+  });
+
+  grunt.registerTask('compile', ['stylus', 'cssmin', 'concat']);
+  grunt.registerTask('compile:min', ['concat', 'uglify']);
+  grunt.registerTask('default', 'compile:min');
+
+};
---
+++ neatline/exhibits/themes/gemini-over-baja-california/show.php
@@ -0,0 +1,29 @@
+<?php
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+?>
+
+<?php echo head(array(
+  'title' => nl_getExhibitField('title'),
+  'bodyclass' => 'neatline show'
+)); ?>
+
+<!-- Exhibit -->
+<div class="exhibit">
+  <?php echo nl_getExhibitMarkup(); ?>
+</div>
+
+<!-- Narrative -->
+<div id="neatline-narrative" class="narrative">
+  <?php echo nl_getExhibitField('narrative'); ?>
+</div>
+
+<?php echo foot(); ?>
