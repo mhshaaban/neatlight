f473ad238a0dfb06a9b289d9f205c69aec23332a
David McClure <david.mcclure@virginia.edu>
2014-04-01 15:26:41 -0700

    Stubs in Sgt. Pepper theme.

---
+++ neatline/exhibits/themes/sgt-pepper/assets/javascripts/layout.js
@@ -0,0 +1,38 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+Neatline.on('start', function() {
+
+  var narrative = $('div.narrative');
+  var exhibit   = $('div.exhibit');
+  var bubble    = $('div.bubble');
+
+  // Cache the width of the narrative.
+  var textWidth = narrative.outerWidth();
+
+  var position = function() {
+
+    var width = $(window).width();
+
+    // Fill width with exhibit.
+    exhibit.outerWidth(width + textWidth);
+
+    // Fill height with content.
+    exhibit.add(narrative).outerHeight($(window).height());
+
+    // Refresh OpenLayers.
+    Neatline.execute('MAP:updateSize');
+
+  };
+
+  $(window).resize(position);
+  position();
+
+});
