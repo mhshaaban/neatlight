8a1b61aa0ad16d7da42e5c535c8d86a330ecbf03
David McClure <davidwilliammcclure@gmail.com>
2014-05-11 12:51:51 -0700

    Adds signer->hometown and hometown->signer mappings.

---
+++ neatline/exhibits/themes/declaration-of-independence/assets/javascripts/lines/hometowns.js
@@ -0,0 +1,143 @@
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-NeatLight
+ * @copyright   2014 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+Neatline.module('Lines', function(Lines) {
+
+
+  Lines.hometowns = {
+
+    // Signer -> Location
+    'button-gwinnett': 'st-catherines-island',
+    'lyman-hall': 'sunbury',
+    'george-walton': 'savannah',
+    'william-hooper': 'wilmington',
+    'joseph-hewes': 'edenton',
+    'john-penn': 'stovall',
+    'edward-rutledge': 'charlestown',
+    'thomas-heyward': 'white-hall-plantation',
+    'thomas-lynch': 'peach-tree-plantation',
+    'arthur-middleton': 'middleton-place',
+    'john-hancock': 'boston',
+    'samuel-chase': 'annapolis',
+    'william-paca': 'wye-plantation',
+    'thomas-stone': 'habre-de-venture',
+    'charles-carroll': 'carrollton-manor',
+    'george-wythe': 'williamsburg',
+    'richard-henry-lee': 'chantilly',
+    'thomas-jefferson': 'monticello',
+    'benjamin-harrison': 'berkeley-plantation',
+    'thomas-nelson': 'yorktown',
+    'francis-lightfoot-lee': 'menokin',
+    'carter-braxton': 'elsing-green',
+    'robert-morris': 'philadelphia',
+    'benjamin-rush': 'philadelphia',
+    'benjamin-franklin': 'philadelphia',
+    'john-morton': 'philadelphia',
+    'george-clymer': 'philadelphia',
+    'james-smith': 'york',
+    'george-taylor': 'easton',
+    'james-wilson': 'carlisle',
+    'george-ross': 'lancaster',
+    'caesar-rodney': 'dover',
+    'george-read': 'new-castle',
+    'thomas-mckean': 'new-castle',
+    'william-floyd': 'brookhaven',
+    'philip-livingston': 'new-york',
+    'francis-lewis': 'new-york',
+    'lewis-morris': 'new-york',
+    'richard-stockton': 'princeton',
+    'john-witherspoon': 'princeton',
+    'francis-hopkinson': 'bordentown',
+    'john-hart': 'hopewell',
+    'abraham-clark': 'roselle',
+    'josiah-bartlett': 'kingston',
+    'william-whipple': 'portsmouth',
+    'samuel-adams': 'boston',
+    'john-adams': 'boston',
+    'robert-treat-paine': 'taunton',
+    'elbridge-gerry': 'marblehead',
+    'stephen-hopkins': 'providence',
+    'william-ellery': 'newport',
+    'roger-sherman': 'new-haven',
+    'samuel-huntington': 'norwich',
+    'william-williams': 'lebanon',
+    'oliver-wolocott': 'litchfield',
+    'matthew-thornton': 'londonderry',
+
+    // Location -> Signer
+    'st-catherines-island': 'button-gwinnett',
+    'sunbury': 'lyman-hall',
+    'savannah': 'george-walton',
+    'wilmington': 'william-hooper',
+    'edenton': 'joseph-hewes',
+    'stovall': 'john-penn',
+    'charlestown': 'edward-rutledge',
+    'white-hall-plantation': 'thomas-heyward',
+    'peach-tree-plantation': 'thomas-lynch',
+    'middleton-place': 'arthur-middleton',
+    'boston': [
+      'john-hancock',
+      'samuel-adams',
+      'john-adams'
+    ],
+    'annapolis': 'samuel-chase',
+    'wye-plantation': 'william-paca',
+    'habre-de-venture': 'thomas-stone',
+    'carrollton-manor': 'charles-carroll',
+    'williamsburg': 'george-wythe',
+    'chantilly': 'richard-henry-lee',
+    'monticello': 'thomas-jefferson',
+    'berkeley-plantation': 'benjamin-harrison',
+    'yorktown': 'thomas-nelson',
+    'menokin': 'francis-lightfoot-lee',
+    'elsing-green': 'carter-braxton',
+    'philadelphia': [
+      'robert-morris',
+      'benjamin-rush',
+      'benjamin-franklin',
+      'john-morton',
+      'george-clymer'
+    ],
+    'york': 'james-smith',
+    'easton': 'george-taylor',
+    'carlisle': 'james-wilson',
+    'lancaster': 'george-ross',
+    'dover': 'caesar-rodney',
+    'new-castle': [
+      'george-read',
+      'thomas-mckean'
+    ],
+    'brookhaven': 'william-floyd',
+    'new-york': [
+      'philip-livingston',
+      'francis-lewis',
+      'lewis-morris'
+    ],
+    'princeton': [
+      'richard-stockton',
+      'john-witherspoon'
+    ],
+    'bordentown': 'francis-hopkinson',
+    'hopewell': 'john-hart',
+    'roselle': 'abraham-clark',
+    'kingston': 'josiah-bartlett',
+    'portsmouth': 'william-whipple',
+    'taunton': 'robert-treat-paine',
+    'marblehead': 'elbridge-gerry',
+    'providence': 'stephen-hopkins',
+    'newport': 'william-ellery',
+    'new-haven': 'roger-sherman',
+    'norwich': 'samuel-huntington',
+    'lebanon': 'william-williams',
+    'litchfield': 'oliver-wolocott',
+    'londonderry': 'matthew-thornton'
+
+  };
+
+
+});
