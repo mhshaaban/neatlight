13c8478f510f8d8e5836d32c45675282454d7ba4
David McClure <davidwilliammcclure@gmail.com>
2014-07-01 20:01:45 -0700

    Code for tutorial.

--- neatline/exhibits/themes/declaration-of-independence/show.php
+++ neatline/exhibits/themes/declaration-of-independence/show.php
@@ -19,36 +19,13 @@
 <!-- Transcription. -->
 <div id="neatline-narrative" class="narrative">
 
-  <header>
-
-    <!-- Title. -->
-    <div class="title">
-      <span>The</span>
-      <code><?php echo htmlspecialchars('<Digital />'); ?></code>
-      <span>Declaration of Independence</span>
-    </div>
-
-    <!-- Credits. -->
-    <div class="credits">
-      <span>By <a
-        href="https://twitter.com/clured"
-        target="_blank">@clured</a></span>,
-      <span>with <a
-        href="http://omeka.org"
-        target="_blank">Omeka</a></span>
-      <span>and <a
-        href="http://neatline.org"
-        target="_blank">Neatline</a></span> &bull;
-      <span><a
-        href="http://dclure.org"
-        target="_blank">dclure.org</a></span>
-    </div>
-
+  <header class="credits">
+    <?php include('credits.php'); ?>
+    <div id="tutorial-button"></div>
   </header>
 
-  <hr />
-
   <?php include('declaration.php'); ?>
+  <?php include('tutorial.php'); ?>
 
 </div>
 
