ce926426f069a1a98bb0529ffa8c05a170f4c45d
David McClure <davidwilliammcclure@gmail.com>
2014-07-11 11:59:32 -0700

    Basic grunt rigging.

---
+++ neatline/exhibits/themes/polyphemus/grunt/uglify.js
@@ -0,0 +1,21 @@
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Neatlight
+ * @copyright   2014 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+module.exports = {
+
+  dist: {
+    files: {
+      'script.js': [
+        'bower_components/nprogress/nprogress.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'assets/javascripts/**/*.js'
+      ]
+    }
+  }
+
+};
