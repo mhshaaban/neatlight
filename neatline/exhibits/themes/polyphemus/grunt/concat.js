3a75f5fcb200aadb614c4eb4b1369c40eca86806
David McClure <davidwilliammcclure@gmail.com>
2014-07-25 20:20:30 -0700

    Toastr popup.

--- neatline/exhibits/themes/polyphemus/grunt/concat.js
+++ neatline/exhibits/themes/polyphemus/grunt/concat.js
@@ -13,10 +13,10 @@ module.exports = {
     src: [
       'bower_components/nprogress/nprogress.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
-      'bower_components/bootstrap-tour/build/js/bootstrap-tour.js',
       'bower_components/nouislider/Link.js',
       'bower_components/nouislider/jquery.nouislider.js',
       'bower_components/mousetrap/mousetrap.js',
+      'bower_components/toastr/toastr.js',
       'assets/javascripts/**/*.js'
     ]
   }
